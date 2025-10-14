1afd992cd4b787d63ba69040fdfcb76ec34816df
ayman f. ali <ayman4swd@gmail.com>
2025-10-14 14:48:38 +0300

    create a new node.js application

---
+++ src/test.sh
@@ -0,0 +1,17 @@
+EXPEXTED = "Hello, Test!"
+
+OUTPUT = $(node e- "console.log(require('./src/app')('Test'))")
+
+if ["$OUTPUT" == "$EXPECTED"]; then 
+
+echo " ✅ Test Passed!"
+
+exit 0
+
+else
+
+echo " ❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+
+exit 1
+
+fi
