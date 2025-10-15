c8fdbd9a434ec728f9b113ad2057acc84ab2cf6d
ayman f. ali <ayman4swd@gmail.com>
2025-10-15 15:51:01 +0300

    push local workflow to github

--- src/test.sh
+++ src/test.sh
@@ -4,13 +4,13 @@ OUTPUT = $(node e- "console.log(require('./src/app')('Test'))")
 
 if ["$OUTPUT" == "$EXPECTED"]; then 
 
-echo " ✅ Test Passed!"
+echo "✅ Test Passed!"
 
 exit 0
 
 else
 
-echo " ❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
 
 exit 1
 
